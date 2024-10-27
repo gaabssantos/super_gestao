1382cdc9d20ff68f4dc46b4e6de6436b4557b4d9
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-27 20:15:55 -0300

    feat(Grouping new routes): :sparkles:

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
+Route::get('/login', function() { return "Login"; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return "Clientes"; });
+    Route::get('/fornecedores', function() { return "Fornecedores"; });
+    Route::get('/produtos', function() { return "Produtos"; });
+});
