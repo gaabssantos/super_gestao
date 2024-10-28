d710f84104eaa8922189ff1537100d64a3049fd6
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-28 19:06:13 -0300

    refactor(Fallback routes): :recycle:

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function() { return "Login"; });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return "Login"; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() { return "Clientes"; });
-    Route::get('/fornecedores', function() { return "Fornecedores"; });
-    Route::get('/produtos', function() { return "Produtos"; });
+    Route::get('/clientes', function() { return "Clientes"; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return "Fornecedores"; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return "Produtos"; })->name('app.produtos');
+});
+
+Route::get('/rota1', function() {
+    echo 'Rota1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::fallback(function() {
+    echo 'A rota acessado não existe. <a href="/">Clique aqui para voltar para a página principal</a>.';
 });
